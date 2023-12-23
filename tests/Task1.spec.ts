0a350a7bc5949a00962a37ab9811379d426e40ca
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2023-12-24 00:10:23 +0300

    task1

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -1,6 +1,6 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import {Blockchain, SandboxContract, TreasuryContract} from '@ton-community/sandbox';
 import { Cell, toNano } from 'ton-core';
-import { Task1 } from '../wrappers/Task1';
+import {ClaimMessage, Task1, UpdateMessage} from '../wrappers/Task1';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
@@ -13,13 +13,20 @@ describe('Task1', () => {
 
     let blockchain: Blockchain;
     let task1: SandboxContract<Task1>;
+    let deployer: SandboxContract<TreasuryContract>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        task1 = blockchain.openContract(Task1.createFromConfig({}, code));
+        deployer = await blockchain.treasury('deployer');
 
-        const deployer = await blockchain.treasury('deployer');
+        task1 = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 123n,
+                executionTime: 4101725062n,
+                receiver: deployer.address,
+                seqno: 100n,
+            }, code));
 
         const deployResult = await task1.sendDeploy(deployer.getSender(), toNano('0.05'));
 
@@ -35,4 +42,59 @@ describe('Task1', () => {
         // the check is done inside beforeEach
         // blockchain and task1 are ready to use
     });
+
+    it("should get seqno", async () => {
+        const value = await task1.getSeqno();
+        console.log("Get seqno gas used:", value.gasUsed)
+        expect(value.out).toEqual(100);
+    });
+
+    it("should get execution time", async () => {
+        const value = await task1.getExecutionTime();
+        console.log("Get execution time gas used:", value.gasUsed)
+        expect(value.out).toEqual(4101725062);
+    });
+
+    it('claim message it is too early', async () => {
+        const result = await task1.sendExternal({
+            $$type: 'ClaimMessage',
+            query_id: 1n,
+        });
+
+        expect(result.transactions).toHaveTransaction({
+            success: false,
+            exitCode: 124,
+            op: 0xbb4be234,
+        })
+    });
+
+    it('claim message it is time', async () => {
+        let contract = blockchain.openContract(
+            Task1.createFromConfig({
+                publicKey: 123n,
+                executionTime: 10000n,
+                receiver: deployer.address,
+                seqno: 100n,
+            }, code));
+
+        const deployResult = await contract.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        const result = await contract.sendExternal({
+            $$type: 'ClaimMessage',
+            query_id: 1n,
+        });
+        console.log("Get execution time gas used:", result)
+        expect(result.transactions).toHaveTransaction({
+            success: true,
+            exitCode: 0,
+            op: 0xbb4be234,
+        })
+        expect(result.transactions).toHaveTransaction({
+            success: true,
+            exitCode: 0,
+            from: contract.address,
+            to: deployer.address,
+        })
+    });
+
 });
--- wrappers/Task1.ts
+++ wrappers/Task1.ts
@@ -1,9 +1,67 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Builder,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+    Slice
+} from 'ton-core';
 
-export type Task1Config = {};
+// storage$_ public_key:uint256 execution_time:uint32 receiver:MsgAddressInt seqno:uint32 = Storage;
+
+export type Task1Config = {
+    publicKey: bigint,
+    executionTime: bigint,
+    receiver: Address,
+    seqno: bigint,
+};
 
 export function task1ConfigToCell(config: Task1Config): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeUint(config.publicKey, 256) // public_key:uint256
+        .storeUint(config.executionTime,32) // execution_time:uint32
+        .storeAddress(config.receiver) // receiver:MsgAddressInt
+        .storeUint(config.seqno, 32) // seqno:uint32
+        .endCell();
+}
+
+// update#9df10277 query_id:uint64 signature:bits512 ^[ locked_for:uint32 new_seqno:uint32 ] = ExtInMsgBody
+export type UpdateMessage = {
+    $$type: 'UpdateMessage';
+    signature: Buffer;
+    seqno: bigint;
+    valid_until: bigint;
+}
+
+// export function storeExtMessage(src: ExtMessage) {
+//     return (builder: Builder) => {
+//         let b_0 = builder;
+//         b_0.storeUint(3240438462, 32);
+//         b_0.storeBuffer(src.signature);
+//         b_0.storeUint(src.seqno, 32);
+//         b_0.storeUint(src.valid_until, 32);
+//         let b_1 = new Builder();
+//         b_1.store(storeSendParameters(src.message_parameters));
+//         b_0.storeRef(b_1.endCell());
+//     };
+// }
+
+// claim#bb4be234 query_id:uint64 = ExtInMsgBody
+export type ClaimMessage = {
+    $$type: 'ClaimMessage';
+    query_id: bigint;
+}
+
+export function storeClaimMessage(src: ClaimMessage) {
+    return (builder: Builder) => {
+        let b_0 = builder;
+        b_0.storeUint(0xbb4be234, 32);
+        b_0.storeUint(src.query_id, 64);
+    };
 }
 
 export class Task1 implements Contract {
@@ -25,5 +83,35 @@ export class Task1 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendExternal(provider: ContractProvider, message: ClaimMessage | Slice) {
+
+        let body: Cell | null = null;
+        if (message && typeof message === 'object' && !(message instanceof Slice) && message.$$type === 'ClaimMessage') {
+            body = beginCell().store(storeClaimMessage(message)).endCell();
+        }
+        if (body === null) { throw new Error('Invalid message type'); }
+
+        await provider.external(body);
+
+    }
+
+    async getSeqno(provider: ContractProvider) {
+        const result = await provider.get('get_seqno', [
+        ]);
+        return {
+            out: result.stack.readNumber(),
+            gasUsed: result.gasUsed,
+        };
+    }
+
+    async getExecutionTime(provider: ContractProvider) {
+        const result = await provider.get('get_execution_time', [
+        ]);
+        return {
+            out: result.stack.readNumber(),
+            gasUsed: result.gasUsed,
+        };
     }
 }
