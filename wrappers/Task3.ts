dfad69e819952427f7bb51013c44a87aa5e34a70
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2023-12-25 02:43:41 +0300

    task3

--- wrappers/Task3.ts
+++ wrappers/Task3.ts
@@ -1,9 +1,25 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Dictionary,
+    Sender,
+    SendMode
+} from 'ton-core';
+import {Maybe} from "ton-core/dist/utils/maybe";
 
 export type Task3Config = {};
 
 export function task3ConfigToCell(config: Task3Config): Cell {
     return beginCell().endCell();
+}
+
+export type MigrationPayload = {
+    new_version: number,
+    migration_code: Maybe<Cell>
 }
 
 export class Task3 implements Contract {
@@ -19,11 +35,20 @@ export class Task3 implements Contract {
         return new Task3(contractAddress(workchain, init), init);
     }
 
+//     ;; Inbound Message Structure
+//     ;; _ new_version:uint32 migration_code:(Maybe ^Cell) = MigrationPayload;
+//     ;; _ expected_version:uint32 new_code:(Maybe ^Cell) migrations:(HashmapE 32 MigrationPayload) payload:^Cell = InternalMsgBody;
+
     async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            body: beginCell()
+                .storeUint(0, 32) // ver 0
+                .storeBit(false) // empty new code
+                .storeDict(Dictionary.empty<number, MigrationPayload>()) // empty migration dict
+                .storeRef(beginCell().endCell()) // empty body
+                .endCell(),
         });
     }
 }
