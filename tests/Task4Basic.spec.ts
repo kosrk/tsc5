54f810356cc4737fef8954dc0534197da3f65fc1
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2023-12-29 04:05:27 +0300

    task4

--- tests/Task4Basic.spec.ts
+++ tests/Task4Basic.spec.ts
@@ -1,11 +1,80 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
-import { Task4Basic } from '../wrappers/Task4Basic';
+import {Cell, toNano, TupleBuilder, Tuple, contractAddress} from 'ton-core';
+import {Task4Basic, Task4BasicConfig, task4BasicConfigToCell} from '../wrappers/Task4Basic';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
 describe('Task4Basic', () => {
     let code: Cell;
+    let x = 88;
+    let d = 46;
+    let q = 63;
+    let s = 83;
+    let e = 69;
+    let p = 33;
+
+    let m1_in: number[][] = [
+        [x,x,x,x,x,x,e,d], // X	X X	X X X E	.
+        [x,x,d,x,x,x,x,d], // X	X .	X X X X	.
+        [x,d,x,d,x,x,x,x], // X	. X	. X X X	X
+        [d,q,x,s,x,x,x,d], // .	? X	S X	X X	.
+        [q,d,x,x,x,x,x,d], // ?	. X	X X	X X	.
+        [x,x,d,d,x,x,x,d], // X	X .	. X	X X	.
+        [x,x,d,d,x,x,q,x], // X	X .	. X	X ?	X
+        [x,x,x,d,d,d,x,x], // X	X X . .	. X	X
+    ];
+    let m1_out: number[][] = [[]];
+    let m1_out_advanced: number[][] = [
+        [x,x,x,x,x,x,e,d], // X	X X	X X X E	.
+        [x,x,d,x,x,x,x,d], // X	X !	X X X X	!
+        [x,d,x,d,x,x,x,x], // X	! X	! X X !	X
+        [d,q,x,s,x,x,x,d], // !	? X	S X	X X	!
+        [q,d,x,x,x,x,x,d], // ?	! X	X X	X X	!
+        [x,x,d,d,x,x,x,d], // X	X !	. X	X X	!
+        [x,x,d,d,x,x,q,x], // X	X .	! X	X !	X
+        [x,x,x,d,d,d,x,x], // X	X X . !	! X	X
+    ];
+
+    let m2_in: number[][] = [
+        [s,x,d,q,x], // S X . ? X
+        [d,x,x,d,x], // . X X . X
+        [x,d,q,d,d], // X . ? . .
+        [d,q,q,d,d], // . ? ? . .
+        [x,q,d,d,d], // X ? . . .
+        [d,d,x,d,x], // . . X . X
+        [d,d,q,d,d], // . . ? . .
+        [x,d,d,d,e], // X . . . E
+    ];
+    let m2_out: number[][] = [
+        [s,x,d,q,x], // S X . ? X
+        [d,p,x,d,x], // ! X X . X
+        [x,p,q,d,d], // X ! ? . .
+        [d,p,q,d,d], // . ! ? . .
+        [x,q,p,d,d], // X ? ! . .
+        [d,d,x,p,x], // . . X ! X
+        [d,d,q,p,d], // . . ? ! .
+        [x,d,d,d,e], // X . . . E
+    ];
+
+    let m1_tb = new TupleBuilder();
+    for (let i= 0; i < m1_in.length; i++) {
+        let row = new TupleBuilder();
+        for (let j= 0; j < m1_in[0].length; j++) {
+            row.writeNumber(m1_in[i][j])
+        }
+        m1_tb.writeTuple(row.build())
+    }
+    let maze1: Tuple = {type: 'tuple', items: m1_tb.build()};
+
+    let m2_tb = new TupleBuilder();
+    for (let i= 0; i < m2_in.length; i++) {
+        let row = new TupleBuilder();
+        for (let j= 0; j < m2_in[0].length; j++) {
+            row.writeNumber(m2_in[i][j])
+        }
+        m2_tb.writeTuple(row.build())
+    }
+    let maze2: Tuple = {type: 'tuple', items: m2_tb.build()};
 
     beforeAll(async () => {
         code = await compile('Task4Basic');
@@ -35,4 +104,12 @@ describe('Task4Basic', () => {
         // the check is done inside beforeEach
         // blockchain and task4Basic are ready to use
     });
+
+    it('solve', async () => {
+        const value = await task4Basic.getSolve(maze1);
+        console.log("Solve gas used:", value.gasUsed)
+        // console.log("Resulting maze:", value.maze)
+        await task4Basic.plotMaze(value.maze)
+    });
+
 });
--- wrappers/Task4Basic.ts
+++ wrappers/Task4Basic.ts
@@ -1,10 +1,30 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+    Tuple, TupleBuilder,
+    TupleReader
+} from 'ton-core';
 
 export type Task4BasicConfig = {};
 
 export function task4BasicConfigToCell(config: Task4BasicConfig): Cell {
     return beginCell().endCell();
 }
+
+const map: { [id: number]: string; } = {
+    [88]: 'X',
+    [46]: '.',
+    [63]: '?',
+    [83]: 'S',
+    [69]: 'E',
+    [33]: '!'
+};
 
 export class Task4Basic implements Contract {
     constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
@@ -25,5 +45,38 @@ export class Task4Basic implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async plotMaze(t: TupleReader) {
+        let res = '';
+        while (t.remaining > 0) {
+            let row = t.readTuple();
+            while (row.remaining > 0) {
+                res = res + map[row.readNumber()] + ' '
+            }
+            res = res + '\n'
+        }
+        console.log("Resulting maze: \n" + res)
+        return;
+    }
+
+    async getSolve(provider: ContractProvider, maze: Tuple) {
+        let n = maze.items.length;
+        let m = 0;
+        if (maze.items[0].type == 'tuple') {
+            m = maze.items[0].items.length
+        }
+        const result = await provider.get('solve', [
+            {type: 'int', value: BigInt(n)},
+            {type: 'int', value: BigInt(m)},
+            maze
+        ]);
+        return {
+            changes: result.stack.readNumber(),
+            obstacles: result.stack.readNumber(),
+            length: result.stack.readNumber(),
+            maze: result.stack.readTuple(),
+            gasUsed: result.gasUsed,
+        };
     }
 }
