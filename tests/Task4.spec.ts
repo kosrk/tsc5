0c416229c138de7afb8d592c402f24539ea0d001
Alexey Kostenko <alexey.a.kostenko@gmail.com>
2024-01-03 00:02:18 +0300

    task4a

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -1,11 +1,124 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
 import { Cell, toNano } from 'ton-core';
-import { Task4 } from '../wrappers/Task4';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+import {buildMaze, Task4} from "../wrappers/Task4";
 
 describe('Task4', () => {
     let code: Cell;
+    let x = 88;
+    let d = 46;
+    let q = 63;
+    let s = 83;
+    let e = 69;
+    let p = 33;
+
+    let m1_in: number[][] = [
+        [x,x,x,x,x,x,e,d], // X	X X	X X X E	.
+        [x,x,d,x,x,x,x,d], // X	X .	X X X X	.
+        [x,d,x,d,x,x,x,x], // X	. X	. X X X	X
+        [d,q,x,s,x,x,x,d], // .	? X	S X	X X	.
+        [q,d,x,x,x,x,x,d], // ?	. X	X X	X X	.
+        [x,x,d,d,x,x,x,d], // X	X .	. X	X X	.
+        [x,x,d,d,x,x,q,x], // X	X .	. X	X ?	X
+        [x,x,x,d,d,d,x,x], // X	X X . .	. X	X
+    ];
+    let m1_out: number[][] = [[]];
+    let m1_out_advanced: number[][] = [
+        [x,x,x,x,x,x,e,d], // X	X X	X X X E	.
+        [x,x,d,x,x,x,x,d], // X	X !	X X X X	!
+        [x,d,x,d,x,x,x,x], // X	! X	! X X !	X
+        [d,q,x,s,x,x,x,d], // !	? X	S X	X X	!
+        [q,d,x,x,x,x,x,d], // ?	! X	X X	X X	!
+        [x,x,d,d,x,x,x,d], // X	X !	. X	X X	!
+        [x,x,d,d,x,x,q,x], // X	X .	! X	X !	X
+        [x,x,x,d,d,d,x,x], // X	X X . !	! X	X
+    ];
+
+    let m1_in_valid: number[][] = [
+        [x,x,x,x,x,x,e,d], // X	X X	X X X E	.
+        [x,x,d,x,x,x,x,d], // X	X .	X X X X	.
+        [x,d,x,d,x,x,d,x], // X	. X	. X X .	X
+        [d,q,x,s,q,q,d,d], // .	? X	S ?	? .	.
+        [x,d,x,x,x,x,x,d], // X	. X	X X	X X	.
+        [x,x,d,d,x,x,x,d], // X	X .	. X	X X	.
+        [x,x,d,d,x,x,q,x], // X	X .	. X	X ?	X
+        [x,x,x,d,d,d,x,x], // X	X X . .	. X	X
+    ];
+
+    let m2_in: number[][] = [
+        [s,x,d,q,x], // S X . ? X
+        [d,x,x,d,x], // . X X . X
+        [x,d,q,d,d], // X . ? . .
+        [d,q,q,d,d], // . ? ? . .
+        [x,q,d,d,d], // X ? . . .
+        [d,d,x,d,x], // . . X . X
+        [d,d,q,d,d], // . . ? . .
+        [x,d,d,d,e], // X . . . E
+    ];
+    let m2_out: number[][] = [
+        [s,x,d,q,x], // S X . ? X
+        [d,p,x,d,x], // ! X X . X
+        [x,p,q,d,d], // X ! ? . .
+        [d,p,q,d,d], // . ! ? . .
+        [x,q,p,d,d], // X ? ! . .
+        [d,d,x,p,x], // . . X ! X
+        [d,d,q,p,d], // . . ? ! .
+        [x,d,d,d,e], // X . . . E
+    ];
+
+    let m2_in_neighbours: number[][] = [
+        [s,x,d,q,x], // S X . ? X
+        [d,e,x,d,x], // . E X . X
+        [x,d,q,d,d], // X . ? . .
+        [d,q,q,d,d], // . ? ? . .
+        [x,q,d,d,d], // X ? . . .
+        [d,d,x,d,x], // . . X . X
+        [d,d,q,d,d], // . . ? . .
+        [x,d,d,d,d], // X . . . .
+    ];
+
+    let m3_in: number[][] = [
+        [x,s], // X S
+        [e,x], // E X
+    ];
+
+    let m4_in: number[][] = [
+        [s,x,d,q], // S . . ?
+        [d,x,x,d], // . ? ? .
+        [d,x,e,d], // X X E .
+    ];
+
+    let m5_in: number[][] = [
+        [s,x], // S X
+        [d,x], // . X
+        [d,x], // . X
+        [q,d], // ? .
+        [d,x], // . X
+        [q,x], // ? X
+        [q,x], // ? X
+        [e,x], // E X
+    ];
+
+    let m6_in: number[][] = [
+        [d,d,d,d,d,d,d,d],
+        [d,q,q,q,q,q,q,d],
+        [d,q,d,d,d,d,q,d],
+        [d,q,e,q,q,d,q,d],
+        [d,q,q,q,q,d,q,d],
+        [d,d,d,d,d,d,q,d],
+        [q,q,q,q,q,q,q,d],
+        [s,d,d,d,d,d,d,d],
+    ];
+
+    let maze1 = buildMaze(m1_in)
+    let maze2 = buildMaze(m2_in)
+    let maze3 = buildMaze(m1_in_valid)
+    let maze4 = buildMaze(m2_in_neighbours)
+    let maze5 = buildMaze(m3_in)
+    let maze6 = buildMaze(m4_in)
+    let maze7 = buildMaze(m5_in)
+    let maze8 = buildMaze(m6_in)
 
     beforeAll(async () => {
         code = await compile('Task4');
@@ -16,6 +129,7 @@ describe('Task4', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
+
 
         task4 = blockchain.openContract(Task4.createFromConfig({}, code));
 
@@ -35,4 +149,74 @@ describe('Task4', () => {
         // the check is done inside beforeEach
         // blockchain and task4 are ready to use
     });
+
+    it('Maze 1', async () => {
+        const value = await task4.getSolve(maze1);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nWalls:", value.changes, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        // expect(value.length).toEqual(0);
+        // expect(value.obstacles).toEqual(0);
+        expect(value.changes).toEqual(1);
+        // expect(value.maze).toEqual(null);
+    });
+
+    it('Maze 2', async () => {
+        const value = await task4.getSolve(maze2);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        expect(value.length).toEqual(7);
+        expect(value.obstacles).toEqual(1);
+        expect(value.changes).toEqual(-1);
+    });
+
+    it('Valid maze 1', async () => {
+        const value = await task4.getSolve(maze3);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        // expect(value.length).toEqual(7);
+        // expect(value.obstacles).toEqual(1);
+        expect(value.changes).toEqual(-1);
+    });
+
+    it('Neighbours', async () => {
+        const value = await task4.getSolve(maze4);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        // expect(value.length).toEqual(7);
+        // expect(value.obstacles).toEqual(1);
+        expect(value.changes).toEqual(-1);
+    });
+
+    it('2x2', async () => {
+        const value = await task4.getSolve(maze5);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        // expect(value.length).toEqual(7);
+        // expect(value.obstacles).toEqual(1);
+        expect(value.changes).toEqual(-1);
+    });
+
+    it('3x4', async () => {
+        const value = await task4.getSolve(maze6);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        expect(value.changes).toEqual(-1);
+    });
+
+    it('8x2', async () => {
+        const value = await task4.getSolve(maze7);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        expect(value.changes).toEqual(-1);
+    });
+
+    it('Maze 6', async () => {
+        const value = await task4.getSolve(maze8);
+        console.log("Distance:", value.length, "\nObstacles in superposition:", value.obstacles, "\nGas used:", value.gasUsed)
+        await task4.plotMaze(value.maze)
+        // expect(value.length).toEqual(7);
+        // expect(value.obstacles).toEqual(1);
+        expect(value.changes).toEqual(-1);
+    });
+
 });
--- wrappers/Task4.ts
+++ wrappers/Task4.ts
@@ -1,9 +1,41 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+    Tuple,
+    TupleBuilder, TupleReader
+} from 'ton-core';
 
 export type Task4Config = {};
 
 export function task4ConfigToCell(config: Task4Config): Cell {
     return beginCell().endCell();
+}
+
+const map: { [id: number]: string; } = {
+    [88]: 'X',
+    [46]: '.',
+    [63]: '?',
+    [83]: 'S',
+    [69]: 'E',
+    [33]: '!'
+};
+
+export function buildMaze(maze: number[][]) :Tuple {
+    let m_tb = new TupleBuilder();
+    for (let i= 0; i < maze.length; i++) {
+        let row = new TupleBuilder();
+        for (let j= 0; j < maze[0].length; j++) {
+            row.writeNumber(maze[i][j])
+        }
+        m_tb.writeTuple(row.build())
+    }
+    return {type: 'tuple', items: m_tb.build()};
 }
 
 export class Task4 implements Contract {
@@ -25,5 +57,42 @@ export class Task4 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async plotMaze(t: TupleReader | null) {
+        if (t == null) {
+            console.log("Not solved.\n")
+            return;
+        }
+        let res = '';
+        while (t.remaining > 0) {
+            let row = t.readTuple();
+            while (row.remaining > 0) {
+                res = res + map[row.readNumber()] + ' '
+            }
+            res = res + '\n'
+        }
+        console.log("Resulting maze: \n" + res)
+        return;
+    }
+
+    async getSolve(provider: ContractProvider, maze: Tuple) {
+        let n = maze.items.length;
+        let m = 0;
+        if (maze.items[0].type == 'tuple') {
+            m = maze.items[0].items.length
+        }
+        const result = await provider.get('solve', [
+            {type: 'int', value: BigInt(n)},
+            {type: 'int', value: BigInt(m)},
+            maze
+        ]);
+        return {
+            changes: result.stack.readNumber(),
+            obstacles: result.stack.readNumber(),
+            length: result.stack.readNumber(),
+            maze: result.stack.readTupleOpt(),
+            gasUsed: result.gasUsed,
+        };
     }
 }
